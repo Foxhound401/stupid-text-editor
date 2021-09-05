783b4d4ea369f758b12ae85c9b27cfb05ce91373
Phuc Phung <phuc.phung@eastplayers.io>
2021-09-05 22:32:49 +0700

    clear screen with append buffer, not writing everytime, print some welcome

--- kilo.c
+++ kilo.c
@@ -2,16 +2,19 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
 /*** defines ***/
+#define KILO_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
 struct editorConfig {
+  int cx, cy;
   int screenrows;
   int screencols;
   struct termios orig_termios;
@@ -49,23 +52,6 @@ void enableRawMode() {
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-void editorRefreshScreen() {
-  // clear the screen by
-  // write `J` command to it ( Erase In Display )
-  // `2` is the argument to clear the entire screen
-  // when clear this leave the cusor at the bottom
-  // of the screen
-  write(STDOUT_FILENO, "\x1b[2J", 4);
-
-  // move the cursor to the top left
-  // [1;1 is the position at the top left
-  // can be omit by default
-  write(STDOUT_FILENO, "\x1b[H", 3);
-
-  editorDrawRows();
-  
-  write(STDOUT_FILENO, "\x1b[H", 3);
-}
 
 /*** terminal ***/
 char editorReadKey() {
@@ -79,28 +65,33 @@ char editorReadKey() {
 }
 
 int getCursorPosition (int *rows, int *cols) {
+  char buf[32];
+  unsigned int i = 0;
+
+  // cursor position report ( get current position of cursor )
   if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
-  printf("\r\n");
-
-  char c;
-  while (read(STDOUT_FILENO, &c, 1) == 1) {
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
+  while (i < sizeof(buf) - 1) {
+    if (read(STDOUT_FILENO, &buf[i], 1) != 1) break;
+    if (buf[i] == 'R') break;
+    i++;
   }
+  
+  buf[i] = '\0';
 
-  editorReadKey();
+  if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+  // pass the 3rd character of buf to sscanf, skip '\x[1b' and '['
+  // passing the string `%d;%d` which tell it to parse two integers
+  // separated by a ; , then input the value to the `rows` and `cols`
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
 
-  return -1;
+  return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
   struct winsize ws;
 
-  if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
     if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
     editorReadKey();
     return getCursorPosition(rows, cols);
@@ -109,6 +100,27 @@ int getWindowSize(int *rows, int *cols) {
     *rows = ws.ws_row;
     return 0;
   }
+}
+
+/*** append buffer ***/
+struct abuf {
+  char *b;
+  int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len) {
+  char *new = realloc(ab->b, ab->len + len);
+
+  if (new == NULL) return;
+  memcpy(&new[ab->len], s, len);
+  ab->b = new;
+  ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+  free(ab->b);
 }
 
 
@@ -126,15 +138,65 @@ void editorProcessKeypress() {
 }
 
 /*** output ***/
-void editorDrawRows() {
+void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+          "kilo -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
+
+    abAppend(ab, "\x1b[K", 3);
+    if (y < E.screenrows -1) {
+      abAppend(ab, "\r\n", 2);
+    }
   }
+}
+
+void editorRefreshScreen() {
+  struct abuf ab = ABUF_INIT;
+
+  abAppend(&ab, "\x1b[?25l", 6);
+  // clear the screen by
+  // write `J` command to it ( Erase In Display )
+  // `2` is the argument to clear the entire screen
+  // when clear this leave the cusor at the bottom
+  // of the screen
+  /* abAppend(&ab, "\x1b[2J", 4); */
+
+  // move the cursor to the top left
+  // [1;1 is the position at the top left
+  // can be omit by default
+  abAppend(&ab, "\x1b[H", 3);
+
+  editorDrawRows(&ab);
+
+  char buf[32];
+  snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
+  abAppend(&ab, buf, strlen(buf));
+  
+  abAppend(&ab, "\x1b[?25h", 6);
+
+  write(STDOUT_FILENO, ab.b, ab.len);
+  abFree(&ab);
 }
 
 /*** init ***/
 void initEditor() {
+  E.cx = 0;
+  E.cy = 0;
+
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
